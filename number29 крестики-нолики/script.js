fa441c7cc369af98f2217486de206212c6e10873
Bedatop <98839707+Bedatop@users.noreply.github.com>
2023-01-30 18:08:05 +0500

    aa

--- number29 крестики-нолики/script.js
+++ number29 крестики-нолики/script.js
@@ -1,8 +1,12 @@
 let cells = document.querySelectorAll('#field td');
+let winner=document.querySelector('.winner');
+let close = document.querySelector('#close');
+let text = document.querySelector('.text');
 console.log(cells);
 
 let i = 0;
 function start(cells) {
+
     for (let cell of cells) {
         cell.addEventListener('click', function step() {
             if (i % 2 == 0) {
@@ -12,28 +16,40 @@ function start(cells) {
             }
             this.removeEventListener('click', step);
             if (isWinner(cells) == true) {
-                alert(`Победитель ${this.textContent}`);
+                winner.style.display = 'flex';
+                text.textContent=`Победитель ${this.textContent}`;
             }
             else if (i==8) {
-                alert('Ничья')
+                winner.style.display = 'flex';
+                text.textContent='Ничья';
             }
             i++
         })
     }
+    close.addEventListener('click', function close() {
+        for (let cell of cells) {
+            cell.textContent = '';
+            winner.style.display = 'none';
+        }
+        i = 0;
+        console.log(i);
+        this.removeEventListener('click', close);
+        start(cells);
+    })
 }
 
 start(cells);
 
 function isWinner(cells) {
     let combs = [
-        [0, 1, 2], // comb
-        [3, 4, 5], // comb
-        [6, 7, 8], // comb
-        [0, 3, 6], // comb
-        [1, 4, 7], // comb
-        [2, 5, 8], // comb
-        [0, 4, 8], // comb
-        [2, 4, 6]  // comb
+        [0, 1, 2], 
+        [3, 4, 5], 
+        [6, 7, 8], 
+        [0, 3, 6], 
+        [1, 4, 7], 
+        [2, 5, 8], 
+        [0, 4, 8], 
+        [2, 4, 6]  
     ]
 
     for (let comb of combs) {
@@ -46,9 +62,4 @@ function isWinner(cells) {
     return false;
 }
 
-// let comb
-// comb = [0, 1, 2]
-// если (cells[0]) == (cells[1]) ==  cells[2]]
-// true
 
-// comb = [3, 4, 5]
